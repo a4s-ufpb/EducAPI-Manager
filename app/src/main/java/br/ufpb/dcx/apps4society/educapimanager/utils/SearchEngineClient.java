0261be588cd0b09ead93626da1f1aed4625e0e9a
vitor <luis.vitor@dce.ufpb.br>
2020-04-28 19:56:00 -0300

    search Engine implemented

--- app/src/main/java/br/ufpb/dcx/apps4society/educapimanager/utils/SearchEngineClient.java
+++ app/src/main/java/br/ufpb/dcx/apps4society/educapimanager/utils/SearchEngineClient.java
@@ -1,46 +1,64 @@
 package br.ufpb.dcx.apps4society.educapimanager.utils;
 
-import android.content.Context;
-import android.widget.Toast;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.JsonHttpResponseHandler;
+import androidx.core.util.Pools;
 
+import com.google.api.client.googleapis.batch.BatchRequest;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.customsearch.Customsearch;
+import com.google.api.services.customsearch.model.Result;
+import com.google.api.services.customsearch.model.Search;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SearchEngineClient {
-    private static final String API_BASE_URL = "https://www.googleapis.com/customsearch/v1?";
     private static final String API_KEY = "AIzaSyBKu8O9msyelwJPRG4hsiMEeW7G2NMjmZw";
     private static final String CX_KEY = "010983324265191686624:wanhkrbv8rc";
-    private AsyncHttpClient client;
 
-    public SearchEngineClient(){
-        this.client = new AsyncHttpClient();
+    private List<Result> resultado = new ArrayList<>();
+
+
+
+
+
+
+    public static void fill(List<Result> foraThread,List<Result> dentro){
+        dentro = foraThread;
     }
 
-    public String getAcessUrl(String relativeUrl){
-        return  API_BASE_URL+relativeUrl;
+    public synchronized List<Result> search(final String query) {
+
+
+            Customsearch.Builder customse = null;
+            try {
+                customse = new Customsearch.Builder(new NetHttpTransport(), new JacksonFactory(), httpRequest -> {
+                    try {
+
+                        httpRequest.setConnectTimeout(20000);
+                        httpRequest.setReadTimeout(20000);
+
+                    } catch (Exception ex) {ex.printStackTrace();}});
+                    } catch (Exception e) {e.printStackTrace();}
+
+
+            try {
+                customse.setApplicationName("EducAPI Manager");
+                Customsearch.Cse.List list = customse.build().cse().list(query);
+                list.setKey(API_KEY);
+                list.setCx(CX_KEY);
+                Search results = list.execute();
+                resultado = results.getItems();
+            } catch (Exception e) {e.printStackTrace();
+
+            }
+
+        return resultado;
     }
 
-    public void search(final String query, int startPage, Context context,JsonHttpResponseHandler handler){
-        try{
-            String url = getAcessUrl("q="+
-                    URLEncoder.encode(query,"utf-8")+
-                    "&start="+
-                    startPage+
-                    "&cx="+
-                    CX_KEY+
-                    "&key="+
-                    API_KEY);
-            client.get(url,handler);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            Toast.makeText(context, "search_not_found", Toast.LENGTH_SHORT).show();
-        }
     }
 
-
-}
