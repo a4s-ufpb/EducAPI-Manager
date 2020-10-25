bb33133bf5b3861619d2cbaf6cf9d5e962aa5638
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2020-10-25 06:44:41 -0300

    Inserindo o tratamento de autenticação na tela de Login

--- app/src/main/java/br/ufpb/dcx/apps4society/educapimanager/view/LoginActivity.java
+++ app/src/main/java/br/ufpb/dcx/apps4society/educapimanager/view/LoginActivity.java
@@ -6,23 +6,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.material.textfield.TextInputLayout;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import br.ufpb.dcx.apps4society.educapimanager.R;
 import br.ufpb.dcx.apps4society.educapimanager.control.facade.CreateObjectFacade;
-import br.ufpb.dcx.apps4society.educapimanager.control.service.RetrofitInitializer;
+import br.ufpb.dcx.apps4society.educapimanager.control.service.response.LoginResponse;
+import br.ufpb.dcx.apps4society.educapimanager.helper.GerenteDeSessao;
+import br.ufpb.dcx.apps4society.educapimanager.helper.RetrofitConfig;
 import br.ufpb.dcx.apps4society.educapimanager.model.bean.Session;
 import br.ufpb.dcx.apps4society.educapimanager.model.dto.UserDTO;
-import br.ufpb.dcx.apps4society.educapimanager.view.ui.home.HomeFragment;
+import br.ufpb.dcx.apps4society.educapimanager.model.dto.UserLogin;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -30,133 +24,71 @@ import retrofit2.Response;
 public class LoginActivity extends AppCompatActivity {
     private Context context = this;
 
-    private Button btn_entrar;
-    private TextInputLayout email_input,password_input;
-    private EditText edtTxEmail,edtTxPassword;
+    private EditText edtTxEmail, edtTxPassword;
     //TODO("implementar 'esqueci minha senha', redirecionar para outra tela onde ira poder fazer a troca da senha")
-    private TextView esqueci_senha,semCadastro;
-    private ArrayList<UserDTO> auth = new ArrayList<>();
     private UserDTO userAuth;
+    private GerenteDeSessao gerenteDeSessao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        pegarUsuarios();
-
         //INICIALIZAÇÃO DOS COMPONENTES
-        btn_entrar = findViewById(R.id.btnLogin);
         edtTxEmail = findViewById(R.id.edtTxEmail);
         edtTxPassword = findViewById(R.id.edtTxPassword);
-        email_input = findViewById(R.id.tilEmail);
-        password_input = findViewById(R.id.tilPassword);
-        semCadastro = findViewById(R.id.withoutRegister);
 
+        gerenteDeSessao = new GerenteDeSessao(this);
+    }
 
-        btn_entrar.setOnClickListener(v -> {
-            if(edtTxEmail.getText().toString().isEmpty() || edtTxPassword.getText().toString().isEmpty()){
-                Toast.makeText(this,"Digite seu email e sua senha",Toast.LENGTH_SHORT).show();
-            }
-            else{
-                try {
-                    if (autorizar(edtTxEmail.getText().toString(), edtTxPassword.getText().toString())) {
-                        Intent next = new Intent();
-                        next.setClass(context, NavDrawerActivity.class);
-                        startActivity(next);
+    public void autenticarUsuario(View view) {
+
+        if (verificarCamposDeTexto()){
+            UserLogin userLogin = new UserLogin(edtTxEmail.getText().toString(), edtTxPassword.getText().toString());
+            Call call = RetrofitConfig.loginService().login(userLogin);
+            call.enqueue(new Callback<LoginResponse>() {
+                @Override
+                public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                    if (response.code() == 200) {
+                        LoginResponse loginResponse = response.body();
+                        gerenteDeSessao.saveAuthToken(loginResponse.getToken());
+                        userAuth = new UserDTO(userLogin.getEmail(), userLogin.getPassword());
+                        CreateObjectFacade.Companion.getInstance().setTempSession(new Session(userAuth));
+                        System.out.println(CreateObjectFacade.Companion.getInstance().getTempSession().getCreator().getEmail());
+                        openNavDrawerActivity();
                     } else {
-                        password_input.setBoxBackgroundColor(getResources().getColor(R.color.crimson));
-                        password_input.setErrorEnabled(true);
-                        password_input.setBoxBackgroundColor(getResources().getColor(R.color.white));
-                        password_input.setError("verifique sua senha e seu email e Tente Novamente");
-
+                        Toast.makeText(LoginActivity.this, "E-mail ou senha incorretos! Verifique e tente novamente.", Toast.LENGTH_LONG).show();
                     }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
                 }
-            }
+
+                @Override
+                public void onFailure(Call<LoginResponse> call, Throwable t) {
+                    Toast.makeText(LoginActivity.this, "Falha na requisição. Tente novamente!", Toast.LENGTH_LONG).show();
+                }
             });
-
-        semCadastro.setOnClickListener(v -> {
-            Intent ir = new Intent();
-            ir.setClass(this.context,RegisterActivity.class);
-            startActivity(ir);
-        });
-    }
-
-    private boolean autorizar (String email, String password) throws InterruptedException {
-        UserDTO tmp = new UserDTO(email,password);
-        for(UserDTO u : this.auth) {
-            if (tmp.getEmail().equals(u.getEmail())) {
-                System.out.println(u.getId());
-                pegarUsuarioId(u.getId());
-                 break;
-            }
-        }
-
-        return validar(tmp);
-
-
-
-    }
-
-    private void pegarUsuarios () {
-        Call call = new RetrofitInitializer().userService().findAll();
-        call.enqueue(new Callback<ArrayList<UserDTO>>() {
-            @Override
-            public void onResponse(Call<ArrayList<UserDTO>> call, Response<ArrayList<UserDTO>> response) {
-                auth = response.body();
-                Toast.makeText(context,"Faça seu Login",Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onFailure(Call<ArrayList<UserDTO>> call, Throwable t) {
-                Toast.makeText(context,"Ocorreu um erro, Por favor, Reinicie o aplicativo. ",Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-  private boolean validar(UserDTO creator){
-        boolean autorizacao = false;
-        try {
-            if (userAuth.equals(creator)) {
-                CreateObjectFacade.Companion.getInstance().setTempSession(new Session(userAuth));
-                autorizacao = true;
-            }
-        }catch(NullPointerException e){
-            Toast.makeText(this,"Algo Deu Errado, Por favor Reinicie o Aplicativo",Toast.LENGTH_LONG).show();
-      }
-        return autorizacao;
-  }
-
-    void pegarUsuarioId(Long id) throws InterruptedException {
-        AuthRunnable g = new AuthRunnable(id);
-        Thread t = new Thread(g);
-        t.start();
-        t.join();
-        userAuth = g.getCreator();
-    }
-
-    private static class AuthRunnable implements Runnable{
-        UserDTO creator;
-        Long id;
-
-        AuthRunnable(Long id){
-            this.id = id;
-        }
-
-        @Override
-        public void run() {
-            Call<UserDTO> call = new RetrofitInitializer().userService().findUser(id);
-            try {
-                creator = call.execute().body();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        UserDTO getCreator() {
-            return creator;
         }
     }
+
+    private void openNavDrawerActivity(){
+        Intent next = new Intent();
+        next.setClass(context, NavDrawerActivity.class);
+        startActivity(next);
+        finish();
+    }
+
+    public void openSemCadastroActivity(View view){
+        Intent ir = new Intent();
+        ir.setClass(context, RegisterActivity.class);
+        startActivity(ir);
+    }
+
+    private boolean verificarCamposDeTexto(){
+        if (edtTxEmail.getText().toString().isEmpty() || edtTxPassword.getText().toString().isEmpty()){
+            Toast.makeText(LoginActivity.this, "Insira o seu e-mail e senha!", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+
+
 }
