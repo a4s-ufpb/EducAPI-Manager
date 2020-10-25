bb9aa6096be29d61360f36139e611b467e0bb85e
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2020-10-25 06:45:25 -0300

    Corrigindo instanciação do FAB

--- app/src/main/java/br/ufpb/dcx/apps4society/educapimanager/view/NavDrawerActivity.kt
+++ app/src/main/java/br/ufpb/dcx/apps4society/educapimanager/view/NavDrawerActivity.kt
@@ -1,14 +1,10 @@
 package br.ufpb.dcx.apps4society.educapimanager.view
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -16,16 +12,10 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import br.ufpb.dcx.apps4society.educapimanager.R
-import br.ufpb.dcx.apps4society.educapimanager.view.ui.challenge.ChallengeFragment
-import br.ufpb.dcx.apps4society.educapimanager.view.ui.challenge.CreateChallengeActivity
-import br.ufpb.dcx.apps4society.educapimanager.view.ui.context.ContextFragment
-import br.ufpb.dcx.apps4society.educapimanager.view.ui.context.CreateContextActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
 
 
-class NavDrawerActivity() : AppCompatActivity(), View.OnClickListener{
+class NavDrawerActivity() : AppCompatActivity(){
 
     private lateinit var appBarConfiguration : AppBarConfiguration
     private lateinit var navController : NavController
@@ -50,13 +40,11 @@ class NavDrawerActivity() : AppCompatActivity(), View.OnClickListener{
 //      menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_contact, R.id.nav_logout, R.id.nav_about
+                R.id.nav_home, R.id.nav_contact, R.id.nav_about, R.id.nav_logout
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-        var fab : FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener(this)
 
     }
 
@@ -69,43 +57,6 @@ class NavDrawerActivity() : AppCompatActivity(), View.OnClickListener{
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-
-    override fun onClick(v: View?) {
-        Log.i(TAG,"CLICK")
-        when(v?.id){
-            R.id.fab -> when (navController.currentDestination){
-                navController.graph.findNode(R.id.nav_home) -> {
-                    if(currentFragmentInHomeViewPage is ContextFragment){
-                        Log.i(TAG,"CONTEXT")
-                        val intent = Intent(applicationContext, CreateContextActivity::class.java)
-                        startActivity(intent)
-                        Log.i(TAG,"CONTEXT")
-                    }else if(currentFragmentInHomeViewPage is ChallengeFragment){
-                        Log.i(TAG,"CHALLENGE")
-                        val intent = Intent(applicationContext, CreateChallengeActivity::class.java)
-                        startActivity(intent)
-                        Log.i(TAG,"CHALLENGE")
-                    }else{
-                        val intent = Intent(applicationContext, CreateContextActivity::class.java)
-                        startActivity(intent)
-                        Log.i(TAG,"CONTEXT")
-                    }
-                }
-            }
-        }
-    }
-
-    companion object {
-        private var currentFragmentInHomeViewPage : Fragment? = null
-
-        fun OnChangeViewPagerFragment(hook : OnNavDrawerListener){
-            currentFragmentInHomeViewPage = hook.onChangeFragment()
-        }
-    }
-
-    interface OnNavDrawerListener{
-        fun onChangeFragment() : Fragment;
     }
 
 }
