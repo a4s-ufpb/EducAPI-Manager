c02deaacc48475d379eca1afd7f4f1ea03cb6ae9
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2020-10-25 06:38:31 -0300

    Alteração no modo como os challenges são listados

--- app/src/main/java/br/ufpb/dcx/apps4society/educapimanager/view/ui/challenge/ChallengeFragment.kt
+++ app/src/main/java/br/ufpb/dcx/apps4society/educapimanager/view/ui/challenge/ChallengeFragment.kt
@@ -1,17 +1,21 @@
 package br.ufpb.dcx.apps4society.educapimanager.view.ui.challenge
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.ufpb.dcx.apps4society.educapimanager.R
-import br.ufpb.dcx.apps4society.educapimanager.control.facade.CreateObjectFacade
-import br.ufpb.dcx.apps4society.educapimanager.control.service.RetrofitInitializer
+import br.ufpb.dcx.apps4society.educapimanager.adapter.ChallengeAdapter
+import br.ufpb.dcx.apps4society.educapimanager.control.service.ChallengesService
+import br.ufpb.dcx.apps4society.educapimanager.control.service.response.ChallengePageResponse
+import br.ufpb.dcx.apps4society.educapimanager.helper.RetrofitConfig
 import br.ufpb.dcx.apps4society.educapimanager.model.bean.Challenge
+import com.omadahealth.github.swipyrefreshlayout.library.SwipyRefreshLayout
+import com.omadahealth.github.swipyrefreshlayout.library.SwipyRefreshLayoutDirection
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,38 +24,68 @@ class ChallengeFragment : Fragment() {
 
     private lateinit var layManager : LinearLayoutManager
     private lateinit var recyclerView: RecyclerView
+    private lateinit var swipyRefreshLayout: SwipyRefreshLayout
+    private lateinit var adapter: ChallengeAdapter
+    private var mutableList : MutableList<Challenge> = mutableListOf()
+    private var PAGE = 0
+    private var LAST_PAGE = true
     private var TAG : String = "CreateFragmentFragmentName"
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_challenge, container, false)
+
+        swipyRefreshLayout = root.findViewById(R.id.swipyrefreshlayout_challenges)
+
         recyclerView = root.findViewById(R.id.challenge_recycle_view)
         layManager = LinearLayoutManager(context)
+        recyclerView.layoutManager = layManager
+        recyclerView.setHasFixedSize(true)
+
+        adapter = ChallengeAdapter(context, mutableList)
+        recyclerView.adapter = adapter
+
+        if (PAGE == 0 && mutableList.size == 0){
+            getChallengesFromService(PAGE)
+        }
+
+        swipyRefreshLayout.setOnRefreshListener { direction ->
+            if (direction == SwipyRefreshLayoutDirection.BOTTOM && !LAST_PAGE){
+                PAGE++
+                getChallengesFromService(PAGE)
+            }else if (direction == SwipyRefreshLayoutDirection.TOP){
+                PAGE = 0
+                mutableList.clear()
+                getChallengesFromService(PAGE)
+            }else{
+                Toast.makeText(context, "Os desafios disponíveis já estão sendo exibidos!", Toast.LENGTH_LONG).show()
+                swipyRefreshLayout.isRefreshing = false
+            }
+        }
+
         return root
     }
 
+    fun getChallengesFromService(page: Int){
+        val call = RetrofitConfig.getPublicRetrofit().create(ChallengesService::class.java)
+            .getChallengesWithPagination(page, null, 10)
+        call.enqueue(object: Callback<ChallengePageResponse> {
+            override fun onResponse(call: Call<ChallengePageResponse>, response: Response<ChallengePageResponse>) {
+                response.body()?.let {
 
-    fun fillRecycleView(challenges : List<Challenge>){
-        recyclerView.layoutManager = layManager
-        recyclerView.adapter = context?.let { ChallengeListAdapter(challenges, it) }
-    }
+                    LAST_PAGE = it.isLast
 
-    fun getChallengesFromService(){
-        val call = RetrofitInitializer().challengeService().findAll()
-        call.enqueue(object: Callback<List<Challenge>?> {
-            override fun onResponse(call: Call<List<Challenge>?>?, response: Response<List<Challenge>?>?) {
-                response?.body()?.let {
-                    fillRecycleView(it)
+                    mutableList.addAll(mutableList.size, it.content)
+                    adapter.notifyItemRangeInserted(mutableList.size, mutableList.size)
+
+                    swipyRefreshLayout.isRefreshing = false
+
                 }
             }
 
-            override fun onFailure(call: Call<List<Challenge>?>?, t: Throwable?) {
-                Log.e(TAG, t?.message)
+            override fun onFailure(call: Call<ChallengePageResponse>, t: Throwable?) {
+                swipyRefreshLayout.isRefreshing = false
             }
         })
     }
 
-    override fun onResume() {
-        super.onResume()
-        getChallengesFromService()
-    }
 }
--- app/src/main/java/br/ufpb/dcx/apps4society/educapimanager/view/ui/challenge/ChallengeListAdapter.kt
+++ app/src/main/java/br/ufpb/dcx/apps4society/educapimanager/view/ui/challenge/ChallengeListAdapter.kt
@@ -1,5 +1,6 @@
 package br.ufpb.dcx.apps4society.educapimanager.view.ui.challenge
 
+import android.content.DialogInterface
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,10 +11,10 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import br.ufpb.dcx.apps4society.educapimanager.R
-import br.ufpb.dcx.apps4society.educapimanager.control.facade.CreateObjectFacade
-import br.ufpb.dcx.apps4society.educapimanager.control.service.RetrofitInitializer
+import br.ufpb.dcx.apps4society.educapimanager.control.service.ChallengesService
+import br.ufpb.dcx.apps4society.educapimanager.helper.RetrofitConfig
 import br.ufpb.dcx.apps4society.educapimanager.model.bean.Challenge
-import br.ufpb.dcx.apps4society.educapimanager.model.bean.Context
+import br.ufpb.dcx.apps4society.educapimanager.model.dto.NewChallengeDTO
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.EncodeStrategy
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -37,8 +38,9 @@ class ChallengeListAdapter(private var challenges : List<Challenge>, private var
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.challenge_name.setText(challenges[position].word)
         loadImage(challenges[position].imageUrl, holder.challenge_image)
-        holder.bnt_edit_challenge.setOnClickListener { dialog(challenges[position]) }
-        holder.excluir_challenge.setOnClickListener { delete(challenges[position].id) }
+        holder.bnt_edit_challenge.setOnClickListener { dialog(challenges[position].id, challenges[position]) }
+        holder.excluir_challenge.setOnClickListener {exibirConfirmacao(challenges[position].id)}
+
     }
 
     private fun loadImage(imageUrl: String?, themeImageLeft: ImageView) {
@@ -49,7 +51,7 @@ class ChallengeListAdapter(private var challenges : List<Challenge>, private var
 
             override fun isResourceCacheable(isFromAlternateCacheKey: Boolean, dataSource: com.bumptech.glide.load.DataSource?, encodeStrategy: EncodeStrategy?): Boolean {
                 return true
-            }
+            }   
 
             override fun decodeCachedResource(): Boolean {
                 return true
@@ -82,7 +84,7 @@ class ChallengeListAdapter(private var challenges : List<Challenge>, private var
 
     }
 
-    private fun dialog (c: Challenge){
+    private fun dialog (idContext:Long, challenge: Challenge){
         val a  = AlertDialog.Builder(fragmentContext)
         a.setTitle("Atualizar Desafio")
         val view = View.inflate(fragmentContext,R.layout.alert_dialog_update_challenge,null)
@@ -95,32 +97,33 @@ class ChallengeListAdapter(private var challenges : List<Challenge>, private var
         val alert = a.create()
         alert.show()
 
-
         btnConfirm.setOnClickListener {
-            if(!edtWord.text.isNullOrEmpty()) c.word = edtWord.text.toString()
-            if(!edtImageUrl.text.isNullOrEmpty()) c.imageUrl = edtImageUrl.text.toString()
-            if(!edtVideoUrl.text.isNullOrEmpty()) c.videoUrl = edtVideoUrl.text.toString()
-            if(!edtSoundUrl.text.isNullOrEmpty()) c.soundUrl = edtSoundUrl.text.toString()
-            update(c)
-            alert.dismiss()
+            if(!edtWord.text.isNullOrEmpty()) challenge.word = edtWord.text.toString()
+            if(!edtImageUrl.text.isNullOrEmpty()) challenge.imageUrl = edtImageUrl.text.toString()
+            if(!edtVideoUrl.text.isNullOrEmpty()) challenge.videoUrl = edtVideoUrl.text.toString()
+            if(!edtSoundUrl.text.isNullOrEmpty()) challenge.soundUrl = edtSoundUrl.text.toString()
 
+            val challengeNewDto : NewChallengeDTO = challenge.challengeDTO(challenge)
+
+            update(idContext, challengeNewDto)
+            alert.dismiss()
         }
 
     }
 
-    private fun update (c:Challenge){
-        val call = RetrofitInitializer().challengeService().update(c,c.id,CreateObjectFacade.instance.tempSession.creator.id)
-        call.enqueue(object : Callback<Void> {
-            override fun onFailure(call: Call<Void>, t: Throwable) {
+    private fun update (idChallenge:Long, challenge:NewChallengeDTO){
+        val call = RetrofitConfig.getAuthRetrofit(fragmentContext).create(ChallengesService::class.java).updateChallenge(idChallenge, challenge)
+        call.enqueue(object : Callback<Challenge> {
+            override fun onFailure(call: Call<Challenge>, t: Throwable) {
                 Toast.makeText(fragmentContext,"Não foi possivel atualizar o Desafio", Toast.LENGTH_SHORT).show()
             }
 
-            override fun onResponse(call: Call<Void>, response: Response<Void>) {
+            override fun onResponse(call: Call<Challenge>, response: Response<Challenge>) {
                 when {
                     response.isSuccessful -> {
                         Toast.makeText(fragmentContext,"Desafio atualizado com sucesso",Toast.LENGTH_SHORT).show()
                     }
-                    response.code() == 403 -> {
+                    response.code() == 401 -> {
                         Toast.makeText(fragmentContext,"Você não pode editar esse desafio",Toast.LENGTH_SHORT).show()
                     }
                     else -> {
@@ -133,7 +136,7 @@ class ChallengeListAdapter(private var challenges : List<Challenge>, private var
     }
 
     private fun delete (id:Long){
-        val call = RetrofitInitializer().challengeService().delete(id,CreateObjectFacade.instance.tempSession.creator.id)
+        val call = RetrofitConfig.getAuthRetrofit(fragmentContext).create(ChallengesService::class.java).deleteChallenges(id)
         call.enqueue(object : Callback<Void>{
             override fun onFailure(call: Call<Void>, t: Throwable) {
                 Toast.makeText(fragmentContext,"Não foi possivel deletar o Desafio", Toast.LENGTH_SHORT).show()
@@ -152,6 +155,24 @@ class ChallengeListAdapter(private var challenges : List<Challenge>, private var
                 }
             }
         })
+    }
+
+
+    private fun exibirConfirmacao(id:Long){
+        val alertDialog  = AlertDialog.Builder(fragmentContext)
+        alertDialog.setTitle("Confirmação")
+        alertDialog.setIcon(null)
+        alertDialog.setMessage("Você realmente deseja apagar esse desafio?")
+        alertDialog.apply {
+            setPositiveButton("Sim", DialogInterface.OnClickListener { dialog, which ->
+                delete(id)
+            })
+
+            setNegativeButton("Não", DialogInterface.OnClickListener { dialog, which ->
+
+            })
+        }
+        alertDialog.show()
     }
 
 }
